351d8c990a3ca40c8831e1ab6b923e1f160f9efb
Ting-Hao Lai <analth355@gmail.com>
2021-08-26 21:23:59 +0800

    First init

---
+++ src/components/AddOrder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    531531
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addOrder',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    details: function () {
+    }
+  },
+  mounted () {
+  }
+}
+</script>
